9f5102d7cba648b3fadc4b53801bc2b9b60f67ab
joshuajhun <joshuajhun@gmail.com>
2016-09-30 12:16:51 -0600

    added formatted dates with moment

--- weather-stubs.js
+++ weather-stubs.js
@@ -1,7 +1,16 @@
+const moment = require('moment')
 const weather = [{
   location: "denver",
   temp: '70',
-  type: 'sunny'
+  type: 'sunny',
+  date: moment('2016-09-20').format("MM-DD-YYYY")
+},
+
+{
+  location: 'denver',
+  temp: '80',
+  type: 'cloudy',
+  date: moment('2016-09-21').format("MM-DD-YYYY")
 }]
 
 module.exports = weather
