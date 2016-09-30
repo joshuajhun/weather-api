206376c45c83e96b318df11d68204f3a46ded28a
joshuajhun <joshuajhun@gmail.com>
2016-09-30 11:35:21 -0600

    cleaned up some of the files

--- server.js
+++ server.js
@@ -6,16 +6,15 @@ const cors            = require('express-cors');
 const port            = process.env.PORT || 8080
 const router          = express.Router();
 const weather         = require('./weather-stubs')
+
 app.locals.title      = 'weather-api'
-app.locals.weather    = []
 app.use('/api', router)
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(cors());
 app.use(bodyParser.json());
 
-router.get('/', function(req, res){
-  debugger;
+router.get('/weather', function(req, res){
   res.json(weather)
 });
 
